700a8f9b1a291aaa107fe58d8604795552264884
AndreaP2A <andrea.porche2a@gmail.com>
2025-02-20 15:57:21 +0000

    Initial commit, ready to publish on npm

---
+++ changelog.md
@@ -0,0 +1,7 @@
+# Changelog
+
+## [1.0.0] - 2025-02-25
+
+### Added
+
+- Initial release of the modal component.
---
+++ src/modalwindow.jsx
@@ -0,0 +1,27 @@
+import PropTypes from "prop-types";
+import "./modalwindow.css";
+
+const Modal = ({ show, onClose, children }) => {
+  if (!show) {
+    return null;
+  }
+
+  return (
+    <div className="modal-overlay">
+      <div className="modal">
+        {children}
+        <button onClick={onClose} className="modal-close-btn">
+          Close
+        </button>
+      </div>
+    </div>
+  );
+};
+
+Modal.propTypes = {
+  show: PropTypes.bool.isRequired,
+  onClose: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Modal;
